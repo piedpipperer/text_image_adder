1db9639653b38d26a6f193aac1e3d8237c3770d3
jordirojo <jrojo@savanamed.com>
2024-02-24 11:18:43 +0100

    adding excel bill creator.

---
+++ excel_creator.py
@@ -0,0 +1,31 @@
+from xlutils.copy import copy
+from xlrd import open_workbook
+
+import xlwt
+import xlrd
+
+combinations = {"2023": list(range(7, 13)), "2024": list(range(1, 3))}
+
+# load excel file
+rb = open_workbook(filename="excels/FACTURA_PIS_703.xls", formatting_info=True)
+
+# open workbook
+# sheet = workbook.active
+
+for year in combinations:
+    for index, month in enumerate(combinations[year]):
+        # year = 2024
+        # month
+
+        wb = copy(rb)
+        w_sheet = wb.get_sheet(0)
+
+        # row number = 0 , colu"mn number = 1
+        w_sheet.write(14, 0, f"L-{year}-{index+1}")
+        w_sheet.write(14, 1, f"01/{month}/{year}")
+
+        for i in range(1, 7):
+            col = w_sheet.col(i)
+            col.width = 256 * 15
+        # save the file
+        wb.save(f"generated_excels/FACTURA_{year}_{month}.xlsx")
