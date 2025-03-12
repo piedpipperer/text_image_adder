72d98cc8261966260d4e606a05e32906c670f6e7
H3llynx <helene.teychene@gmail.com>
2025-03-12 13:17:41 +0100

    miau

--- excel_creator.py
+++ excel_creator.py
@@ -2,23 +2,23 @@ from xlutils.copy import copy
 from xlrd import open_workbook
 from xlwt import XFStyle
 from argparse import ArgumentParser
+import os
 
-combinations = {"2024": list(range(1, 6))}
-combinations2 = {"2024": list(range(7, 13)), "2025": list(range(1, 4))}
-new_combinations = [{"date_interval": combinations, "importe": 204}, {"date_interval": combinations2, "importe": 208}]
-
-rb = open_workbook(filename="excels/FACTURA_PIS_703.xls", formatting_info=True)
+COMBINATIONS = {"2024": list(range(1, 6))}
+COMBINATIONS2 = {"2024": list(range(7, 13)), "2025": list(range(1, 4))}
+NEW_COMBINATIONS = [{"date_interval": COMBINATIONS, "importe": 204}, {"date_interval": COMBINATIONS2, "importe": 208}]
 
 
 def generate_invoices(year_filter=None, month_filter=None):
-    for combination in new_combinations:
+    rb = open_workbook(filename="excels/FACTURA_PIS_703.xls", formatting_info=True)
+    wb = copy(rb)
+    w_sheet = wb.get_sheet(0)
+    for combination in NEW_COMBINATIONS:
         for year in combination["date_interval"]:
             for index, month in enumerate(combination["date_interval"][year]):
                 if (year_filter is None or year_filter == str(year)) and (
                     month_filter is None or month_filter == str(month)
                 ):
-                    wb = copy(rb)
-                    w_sheet = wb.get_sheet(0)
                     w_sheet.write(14, 0, f"L-{year}-{index+1}")
                     w_sheet.write(14, 1, f"01/{month}/{year}")
                     style = XFStyle()
@@ -29,7 +29,10 @@ def generate_invoices(year_filter=None, month_filter=None):
                     for i in range(1, 7):
                         col = w_sheet.col(i)
                         col.width = 256 * 15
-                    wb.save(f"generated_excels/FACTURAS/{year}-{month}.xlsx")
+                    year_folder = f"generated_excels/FACTURAS/{year}"
+                    if not os.path.exists(year_folder):
+                        os.makedirs(year_folder)
+                    wb.save(f"{year_folder}/{month}.xlsx")
 
 
 if __name__ == "__main__":
